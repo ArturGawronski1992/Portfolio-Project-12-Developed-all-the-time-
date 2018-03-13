c52aed84dcf27fce6b962c71ab9ac13b9bee18e0
Artur Gawroński <artur.gawronski7@gmail.com>
2018-03-13 17:19:57 +0100

    pliki

---
+++ script.js
@@ -0,0 +1,136 @@
+alert('Witamy w teleturnieju Familiada');
+
+var word1 = 'ROSJA';
+var word2 = 'USA';
+var word3 = 'KANADA';
+var word4 = 'CHINY';
+var word5 = 'AUSTRALIA';
+var word6 = 'BRAZYLIA';
+
+
+function familiada() {
+
+var chance = 1;
+var odp;
+
+odp = prompt('Wymień największe kraje świata');
+
+	if (odp == word1 || odp == word2 || odp == word3 || odp == word4 || odp == word5 || odp == word6) {
+	alert('Ping');
+		if (odp == word1) {
+		alert('Zdobywasz 26 punktów');				
+		} else if (odp == word2) {
+		alert('Zdobywasz 22 punktów');
+		} else if (odp == word3) {
+		alert('Zdobywasz 18 punktów');
+		} else if (odp == word4) {
+		alert('Zdobywasz 13 punktów');
+		} else if (odp == word5) {
+		alert('Zdobywasz 9 punktów');
+		} else if (odp == word5) {
+		alert('Zdobywasz 6 punktów');
+		}
+	} else {
+	alert('Pum');
+		while (chance > 0) {
+		alert('Straciłeś 1 szansę');
+		chance = chance - 1;
+		}
+	}	
+}
+function familiada2() {
+
+var chance = 1;
+var odp;
+
+odp = prompt('Wymień największe kraje świata');
+
+	if (odp == word1 || odp == word2 || odp == word3 || odp == word4 || odp == word5 || odp == word6) {
+	alert('Ping');
+		if (odp == word1) {
+		alert('Zdobywasz 26 punktów');				
+		} else if (odp == word2) {
+		alert('Zdobywasz 22 punktów');
+		} else if (odp == word3) {
+		alert('Zdobywasz 18 punktów');
+		} else if (odp == word4) {
+		alert('Zdobywasz 13 punktów');
+		} else if (odp == word5) {
+		alert('Zdobywasz 9 punktów');
+		} else if (odp == word5) {
+		alert('Zdobywasz 6 punktów');
+		}
+	} else {
+	alert('Pum');
+		while (chance > 0) {
+		alert('Straciłeś 2 szansę');
+		chance = chance - 1;
+		}
+		alert('Narada drużyny przeciwnej');
+	}	
+}	
+function familiada3() {
+
+var chance = 1;
+var odp;
+
+odp = prompt('Wymień największe kraje świata');
+
+	if (odp == word1 || odp == word2 || odp == word3 || odp == word4 || odp == word5 || odp == word6) {
+	alert('Ping');
+		if (odp == word1) {
+		alert('Zdobywasz 26 punktów');				
+		} else if (odp == word2) {
+		alert('Zdobywasz 22 punktów');
+		} else if (odp == word3) {
+		alert('Zdobywasz 18 punktów');
+		} else if (odp == word4) {
+		alert('Zdobywasz 13 punktów');
+		} else if (odp == word5) {
+		alert('Zdobywasz 9 punktów');
+		} else if (odp == word5) {
+		alert('Zdobywasz 6 punktów');
+		}
+	} else {
+	alert('Pum');
+		while (chance > 0) {
+		alert('Straciłeś 3 szansę');
+		chance = chance - 1;
+		}
+		alert('Szansa dla drużyny przeciwnej');
+	}	
+}	
+function familiada4() {
+
+var chance = 1;
+var odp;
+
+odp = prompt('Wymień największe kraje świata');
+
+	if (odp == word1 || odp == word2 || odp == word3 || odp == word4 || odp == word5 || odp == word6) {
+	alert('Ping');
+		if (odp == word1) {
+		alert('Zdobywasz 26 punktów');				
+		} else if (odp == word2) {
+		alert('Zdobywasz 22 punktów');
+		} else if (odp == word3) {
+		alert('Zdobywasz 18 punktów');
+		} else if (odp == word4) {
+		alert('Zdobywasz 13 punktów');
+		} else if (odp == word5) {
+		alert('Zdobywasz 9 punktów');
+		} else if (odp == word5) {
+		alert('Zdobywasz 6 punktów');
+		}
+	} else {
+	alert('Pum');
+	alert('Drużyna przeciwna przejmuje punkty w tej rundzie');
+	}	
+}	
+
+familiada()
+familiada2()
+familiada3()
+familiada4()
+
+
